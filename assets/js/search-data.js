827681eb804225a1b4d008bcd1bdd96cc1b3a15c
liuyufei-nubot <1320264328@qq.com>
2025-10-28 09:54:38 +0000

    Deploying to gh-pages from @ liuyufei-nubot/liuyufei-nubot.github.io@a790fb2bc7c8a3922b77e77ba84447600b9582d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-paper-accepted-to-ral-25",
+          title: '🎉 One paper accepted to RAL’25.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
